d78515ea69e35031d4040148e9c5992ee9f0a5f5
banthoon <banthoon.git@gmail.com>
2025-06-04 21:58:09 +0700

    update header page

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
         transition={{ duration: 0.8, delay: 0.5 }}
         className="text-3xl sm:text-6xl lg:text-[66px] font-[Ovo]"
       >
-        Engineer IT
+        Engineer IT Developer
       </motion.h1>
 
       <motion.p
