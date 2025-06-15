ca38aa0700a269c62e28908e2f4058843c5c0ba6
banthoon <banthoon.git@gmail.com>
2025-06-15 13:51:50 +0700

    update web

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -58,11 +58,11 @@ const Header = () => {
           initial={{ y: 30, opacity: 0 }}
           whileInView={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.6, delay: 1.2 }}
-          href="/sample-resume.pdf"
+          href="/resume-banthoon.pdf"
           download
           className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black"
         >
-          รีซูมี <Image src={assets.download_icon} alt="" className="w-4" />
+          resume <Image src={assets.download_icon} alt="" className="w-4" />
         </motion.a>
       </div>
     </div>
